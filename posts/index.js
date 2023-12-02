5e7f9d292d0de8ac3958aa986d494f30e27a81a4
Marwan-G <ghabin2004@gmail.com>
2023-12-02 16:22:20 +0000

    setup posts service

--- posts/index.js
+++ posts/index.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const app = express();
-const randomBytes = require('crypto');
+const crypto = require('crypto');
 app.use(express.json());
 const posts = {};
 app.get('/posts', (req, res) => {
-
-})
+    res.json({"testpost": "res from /posts"})
+});
 app.post('/posts', (req, res,) => {
-    const id = randomBytes(4).toString('hex')
+    const id = crypto.randomBytes(4).toString('hex')
     const {title} = req.body;
     posts[id] = {
-        id, title
+        id,  // Shorthand for id: id
+        title  // Shorthand for id: id
     };
+    console.log(posts[id])
+    res.status(201).json(posts[id])
 });
 
 app.listen(4000, () => {
