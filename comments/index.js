7464e898e1827bf1b0ff7d30122e1c9bdf89f213
Marwan-G <ghabin2004@gmail.com>
2023-12-10 17:11:40 +0000

    update response

--- comments/index.js
+++ comments/index.js
@@ -6,8 +6,7 @@ app.use(express.json())
 const commentsByPostId = {};
 app.get('/posts/:id/comments', (req, res, next) => {
     const comments = commentsByPostId[req.params.id] || [];
-    res.send(comments);
-
+    res.send({"allcomments": comments})
 })
 app.post('/posts/:id/comments', (req, res, next) => {
     const commentId = crypto.randomBytes(4).toString('hex');
@@ -16,7 +15,8 @@ app.post('/posts/:id/comments', (req, res, next) => {
     const comments = commentsByPostId[req.params.id] || [];
     comments.push({"id": commentId, content});
     commentsByPostId[req.params.id] = comments;
-    res.send(comments)
+    // res.json({"commentsByPostId": commentsByPostId})
+    res.send(commentsByPostId)
 })
 
 app.listen(4001, () => console.log("comments service listen on Port 4001"))
--- posts/index.js
+++ posts/index.js
@@ -9,14 +9,11 @@ app.get('/posts', (req, res) => {
 app.post('/posts', (req, res,) => {
     const id = crypto.randomBytes(4).toString('hex')
     const {title} = req.body;
-    console.log("amazing marwan mussa ghubein");
-  
     posts[id] = {
-        id,  // Shorthand for id:
-        title  // Shorthand for id: id
+        id,  // Shorthand for id:id
+        title  // Shorthand for title:title
     };
-
-    res.status(201).json(posts[id])
+    res.status(201).json(posts);
 });
 
 app.listen(4000, () => {
