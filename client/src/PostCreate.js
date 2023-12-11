de4a49d4431b45fbcf43b7fa22dd98e5b23a3b57
Marwan-G <ghabin2004@gmail.com>
2023-12-11 16:27:40 +0000

    remove deprecated syntax

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default () => {
-    return <div> Blog App From component hhhh</div>;
+    return <div>
+
+    </div>;
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDome from 'react-dom';
+import {createRoot} from "react-dom/client";
 import App from './App'
 
-ReactDome.render(
-    <App/>, document.getElementById('root')
-)
+// ReactDome.render(
+//     <App/>, document.getElementById('root')  # deprecated syntax
+// )
+createRoot(document.getElementById('root')).render(<App/>) //from version >18 use this syntax
