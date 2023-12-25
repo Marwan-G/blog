e64a34b680ee6ad2cf8bed673837f43eb3add7a9
Marwan-G <ghabin2004@gmail.com>
2023-12-25 19:13:37 +0000

    use query service

--- query/index.js
+++ query/index.js
@@ -1,51 +1,38 @@
-const express = require("express");
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+const app = express();
+app.use(bodyParser.json());
 app.use(cors());
-app.use(express.json());
-posts = {};
+
+const posts = {};
 
 app.get('/posts', (req, res) => {
-    res.send(posts)
+    res.send(posts);
 });
-// {
-//     "91f2d63b": {
-//     "id": "963b",
-//         "title": "this is second  post",
-//         "comments": [{ "id": "ec9","content": "testComment"}]
-//    }
-// }
-// {
-//     type: "CommentCreated",
-//         data: {
-//     commentId,
-//         content,
-//         postId: req.params.id
-// }
-// }
-// //post
-// data = {
-//     type: "PostCreated",
-//     data: {
-//         id,
-//         title
-//     },
-// }
+
 app.post('/events', (req, res) => {
     const {type, data} = req.body;
-    if (type === "PostCreated") {
-        const {id, title} = data
-        posts[id] = {id, title, comments: []}
+
+    if (type === 'PostCreated') {
+        const {id, title} = data;
+
+        posts[id] = {id, title, comments: []};
     }
-    if (type === "CommentCreated") {
-        const {commentId, content, postId} = {data}
-        const post = posts[postId]
-        post.comments.push({commentId, content})
+
+    if (type === 'CommentCreated') {
+        const {commentId, content, postId} = data;
+
+        const post = posts[postId];
+        post.comments.push({commentId, content});
     }
-    console.log(posts, post)
+
+    console.log(posts);
+
+    res.send({});
 });
 
 app.listen(4002, () => {
-    console.log("Query service listening on Port 4002")
+    console.log('Listening on 4002');
 });
